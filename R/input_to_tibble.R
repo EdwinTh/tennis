81547ee3cda0d9849b4b92659ae9493c8ae456e9
EdwinTh <edwinthoen@hotmail.com>
2017-03-02 21:56:40 +0100

    start with processing data

---
+++ R/input_to_tibble.R
@@ -0,0 +1,24 @@
+# Bind all the inidividual csv files to tibbles, so we have a complete table
+# for each subject
+read_tennis_data <- function(year,
+                             file = "atp_matches",
+                             subdir = "tennis_atp") {
+  readr::read_csv(
+    sprintf(
+      "~/Documents/R_packages/tennis/data_input/%s/%s_%d.csv",
+       subdir, file, year))
+}
+
+atp_matches <- purrr::map(1968:2017, read_tennis_data) %>% do.call("rbind", .)
+
+atp_matches_futures <-
+  purrr::map(1991:2017, read_tennis_data, file = "atp_matches_futures") %>%
+  do.call("rbind", .)
+
+atp_matches_qual_chal <-
+  purrr::map(1991:2017, read_tennis_data, file = "atp_matches_qual_chall") %>%
+  do.call("rbind", .)
+
+atp_players <- readr::read_csv(
+  "~/Documents/R_packages/tennis/data_input/tennis_atp/atp_players.csv",
+  col_names = FALSE)
