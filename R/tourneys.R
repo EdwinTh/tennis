5324b0ebe09f85785e683c9e3d987968fd996a41
EdwinTh <edwinthoen@hotmail.com>
2017-03-20 20:39:55 +0100

    Refactored time and tourneys

--- R/tourneys.R
+++ R/tourneys.R
@@ -1,41 +1,100 @@
+#' Filter Tennis Data on Tournament
+#'
+#' Select one or more tournaments from \code{x}.
+#' @param x A data frame from the \code{tennis} package.
+#' @param ... Either the name or the id of the tournament(s). Might
+#' be used together. The ids are formed by the last three characters
+#' of \code{tourney_id}.
+#' @param x A data frame from the \code{tennis} package.
+#' @param ... A vector with the ids and/or tournaments.
+#' @return A subset of \code{x} with the matches of the requested tournaments.
+#' @examples
+#' # find all the Grand Slam matches in 2016
+#' library(dplyr)
+#' atp_matches %>% filter_time(2016) %>%
+#'     filter_tourney("Australian Open", "Roland Garros", "Wimbledon", "US Open")
+#'
+#' find all the Grand Slam matches from 2010 to 2015
+#' atp_matches %>% filter_time(2010, 2015) %>%
+#'    filter_tourney(520, 540, 560, 580)
+#'
+#' find all matches at Sydney, Brisbane and Australian Open
+#' atp_matches %>% filter_tourney("Sydney", "Brisbane", 580)
+#' @export
+filter_tourney <- function(x, ... ) {
+  check_x_tourney(x)
+  tourneys <- get_ddd(as.list(match.call()))
 
-find_tourney <- function(x, ... ) {
+  tourney_names <- extract_name(x)
+  tourney_ids   <- extract_ids(x)
+
+  full_set <- filter_tourneys(x, tourney_names, tourney_ids, tourneys)
+  check_tourneys_found(full_set, tourneys)
+  return(full_set)
+}
+
+check_x_tourney <- function(x){
   stopifnot(is.data.frame(x))
-  stopifnot("tourney_id" %in% colnames(x))
+  name_missing <- !"tourney_name" %in% colnames(x)
+  id_missing   <- !"tourney_id" %in% colnames(x)
 
-  fun_args <- as.list(match.call())
-  tourneys <- tolower(as.character(fun_args[3:length(fun_args)]))
-
-  # remove davis cup matches, because they don't have an ID
-  if ("tourney_level" %in% colnames(x)) {
-    x <- dplyr::filter(x, tourney_level != 'D')
+  if (name_missing & !id_missing) {
+    warning("tourney_name is missing in x, can only use tourney_id")
+  } else if (!name_missing & id_missing) {
+    warning("tourney_id is missing in x, can only use tourney_name")
+  } else if (name_missing & id_missing) {
+    stop("Both tourney_id and tourney_name are missing in x", call. = FALSE)
   }
+}
 
-  tourney_ids <- tolower(get_tourney_id(x))
-  tourney_names <- tolower(x$tourney_name)
+get_tourney_id <- function(x) {
+  splitted <- strsplit(x$tourney_id, '-')
+  ids <- sapply(splitted, function(x) x[2])
+  ids_as_num <- suppressWarnings(as.numeric(ids))
+  ids[is.na(ids_as_num)] <- NA
+  ids
+}
 
-  ind <- (tourney_names %in% tourneys) |
-    (tourney_ids %in% tourneys)
-  full_set <- x[ind, ]
+extract_ids <- function(x) {
+  if ("tourney_id" %in% colnames(x)) {
+    return(tolower(get_tourney_id(x)))
+  } else {
+    return(NA)
+  }
+}
 
-  found <- unique(c(tourney_names[ind], tourney_ids[ind]))
-  missing_names <- tourneys[! tourneys %in% found]
+extract_names <- function(x) {
+  if ("tourney_name" %in% colnames(x)) {
+    return(tolower(x$tourney_name))
+  } else {
+    return(NA)
+  }
+}
+
+filter_tourneys <- function(x, nm, id, tourneys) {
+  nm_found <- nm %in% tolower(tourneys)
+  id_found <- id %in% tourneys
+  ind <- nm_found | id_found
+  return(x[ind, ])
+}
+
+check_tourneys_found <- function(full_set, tourneys){
+  if ("tourney_id" %in% colnames(full_set) ) {
+    ids <- get_tourney_id(full_set)
+  } else {
+    ids <- NA
+  }
+  if ("tourney_name" %in% colnames(full_set) ) {
+    names <- tourney_names
+  } else {
+    names <- NA
+  }
+  found <- unique(c(tolower(names), ids))
+  missing_names <- tourneys[! tolower(tourneys) %in% found]
 
   if (length(missing_names) > 0) {
     for(n in missing_names) {
       warning(sprintf("Could not find %s", n), call. = FALSE)
     }
   }
-
-  return(full_set)
-
 }
-find_tourney(atp_matches, "580", "wimbledon", "roland garros", "Jos")
-
-get_tourney_id <- function(df) {
-  splitted <- strsplit(df$tourney_id, '-')
-  sapply(splitted, function(x) x[2])
-}
-
-# function that will distill from draw_size and the match_num
-# what the round of the match was within the tournament.
--- R/years.R
+++ R/years.R
@@ -1,14 +1,43 @@
-
+#' Filter Tennis Data on Time
+#'
+#' Return all matches in \code{x} that were played between two dates.
+#' Matches played on \code{to} or \code{from} are included in te returned data.
+#' @param x A data frame from the \code{tennis} package.
+#' @param from A vector of length one that indicates the starting point.
+#' See Details.
+#' @param to A vector of length one that indicates the end point.
+#' See Details.
+#' @return The subset in \code{x} of all the matches that were played
+#' between \code{from} and \code{to}.
+#' @details The values for \code{from} and \code{to} are compared to the
+#' integer field \code{tourney_date} in \code{x}. When they are nonnumeric,
+#' for instance of class \code{Date}, they are cleaned and coerced to integer.
+#' \code{from} and \code{to} may contain less than eight characters, they will
+#' padded if necesarry.
+#' @examples
+#' # get all matches in 2016
+#' filter_time(atp_matches, 2016)
+#'
+#' # get all matches from tourneys that start in June 2016
+#' filter_time(atp_matches, 201606)
+#'
+#' # get all matches from tourneys started between
+#' # June 2015 and May 2016
+#' filter_time(atp_matches, "2015-06", "2016-05")
+#' @export
 filter_time <- function(x, from, to = from) {
-  stopifnot(is.data.frame(x))
-  stopifnot("tourney_date" %in% colnames(x))
-  from <- check_integer(from); to <- check_integer(to)
-  from <- as.integer( substr( paste(from, "0000000", sep = ''), 1, 8) )
-  to <- as.integer( substr( paste(to, "9999999", sep = ''), 1, 8) )
-  dplyr::filter(x,tourney_date >= from, tourney_date <= to)
+  check_x_time(x)
+  from <- create_from_to(from, "from")
+  to   <- create_from_to(to, "to")
+  dplyr::filter(x, tourney_date >= from, tourney_date <= to)
 }
 
-check_integer <- function(x) {
+check_x_time <- function(x) {
+  stopifnot(is.data.frame(x))
+  stopifnot("tourney_date" %in% colnames(x))
+}
+
+ensure_integer <- function(x) {
   if(!is.numeric(x)) {
     numerics <-
       paste(unlist(
@@ -16,9 +45,34 @@ check_integer <- function(x) {
         collapse = '')
     as.integer(numerics)
   } else {
-    x
+    ret <- x
   }
 }
 
+check_from_to <- function(x) {
+  if (is.na(x) | nchar(x) > 8) {
+    stop("Invalid value for from or to", call. = FALSE)
+  }
+}
 
-atp_matches_qual_chal %>% filter_time(from = 2010, to = as.Date("2010-06-01"))
+round_data <- function(x, direction = c("from", "to")) {
+  direction <- match.arg(direction)
+  if (direction == "from") {
+    as.integer( substr( paste(x, "0000000", sep = ''), 1, 8) )
+  } else {
+    as.integer( substr( paste(x, "9999999", sep = ''), 1, 8) )
+  }
+}
+
+create_from_to <- function(x, which = c("from", "to")) {
+  which <- match.arg(which)
+  if (!is.na(x)) {
+    x <- ensure_integer(x)
+    x <- check_from_to(x)
+    ret <- round_data(x, which)
+  } else {
+    ret <- ifelse(which == "from", 0, 999999999)
+  }
+  return(ret)
+}
+
